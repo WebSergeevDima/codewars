29454120472b93b7859f36abf6b126c1fcf90305
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-01-29 23:54:37 +0300

    Thinkful - Logic Drills: Traffic light

---
+++ 50.js
@@ -0,0 +1,15 @@
+function updateLight(current) {
+
+    if (current === 'green') {
+        return 'yellow';
+    }
+
+    if (current === 'yellow') {
+        return 'red';
+    }
+
+    if (current === 'red') {
+        return 'green';
+    }
+
+}
