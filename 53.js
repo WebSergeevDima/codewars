a857ea8c5c562de22e2c3c9009bf951c97bcd024
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-01-30 17:21:37 +0300

    Multiplication table for number

---
+++ 53.js
@@ -0,0 +1,12 @@
+function multiTable(number) {
+    let str = '';
+    for (let i = 1; i <= 10; i++) {
+        str += `${i} * ${number} = ${i * number}`;
+
+        if (i != 10) {
+            str += '\n';
+        }
+    }
+
+    return str;
+}
